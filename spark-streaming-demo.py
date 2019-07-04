083503f54c418960b6293fcb05a80d788fbfa93f
4Linux - Free Software Solutions <construtores@4linux.com.br>
2019-07-04 08:31:34 -0300

    Add files via upload

---
+++ spark-streaming-demo.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+# Create a local StreamingContext with two working threads and a batch interval of 2 seconds
+sc = SparkContext("local[2]", "NetworkWordCount")
+ssc = StreamingContext(sc, 2)
+
+# Create a DStream
+lines = ssc.socketTextStream("sandbox-hdp.hortonworks.com", 3333)
+
+# Split each line into words
+words = lines.flatMap(lambda line: line.split(" "))
+
+# Count each word in each batch
+pairs = words.map(lambda word: (word, 1))
+wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+# Print each batch
+wordCounts.pprint()
+
+ssc.start()             # Start the computation
+ssc.awaitTermination()  # Wait for the computation to terminate
